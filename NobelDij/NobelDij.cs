cf95dc5ab9dc2c6312f0a9e0968fb511fbf713fd
stigibalint <72031994+stigibalint@users.noreply.github.com>
2023-06-01 11:08:50 +0200

    Add files via upload

---
+++ NobelDij/NobelDij.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NobelDij
+{
+    public class NobelDij
+    {
+        int evszam;
+        string tipus;
+        string keresztNev;
+        string vezetekNev;
+
+        public NobelDij(int evszam, string tipus, string keresztNev, string vezetekNev)
+        {
+            this.Evszam = evszam;
+            this.Tipus = tipus;
+            this.KeresztNev = keresztNev;
+            this.VezetekNev = vezetekNev;
+        }
+        public NobelDij(string CSVsorok)
+        {
+            var mezo = CSVsorok.Split(';');
+            this.evszam = int.Parse(mezo[0]);
+            this.tipus = mezo[1];
+            this.keresztNev = mezo[2];
+            this.vezetekNev = mezo[3];
+        }
+        public int Evszam { get => evszam; set => evszam = value; }
+        public string Tipus { get => tipus; set => tipus = value; }
+        public string KeresztNev { get => keresztNev; set => keresztNev = value; }
+        public string VezetekNev { get => vezetekNev; set => vezetekNev = value; }
+    }
+}
--- NobelDij/Program.cs
+++ NobelDij/Program.cs
@@ -3,13 +3,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Linq;
+using System.IO;
 namespace NobelDij
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            List<NobelDij> adatok = new List<NobelDij>();
+            try
+            {
+                File.ReadAllLines("nobel.csv").Skip(1).ToList().ForEach(x => adatok.Add(new NobelDij(x)));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Hiba a fájl beolvasásakor {e}");
+                throw;
+            }
+            var artur = adatok.Where(x => x.VezetekNev == "McDonald" && x.KeresztNev == "Arthur B.");
+            foreach (var item in artur)
+            {
+                Console.WriteLine($"3. feladat: {item.Tipus}");
+            }
+            var nobeldij2017 = adatok.Where(x => x.Evszam == 2017).ToList();
+            foreach (var nobel in nobeldij2017)
+            {
+                Console.WriteLine($"4. feladat: {nobel.VezetekNev} {nobel.KeresztNev}");
+            }
+            var napjainkban = adatok.Where(x => x.Evszam > 1990 && x.Tipus == "béke").ToList();
+            Console.WriteLine("5. feladat:");
+            foreach (var item in napjainkban)
+            {
+                Console.WriteLine($"{item.Evszam} {item.VezetekNev}");
+            }
+            var curieCsalad = adatok.Where(x => x.VezetekNev.Contains("Curie")).ToList();
+            Console.WriteLine("6. feladat:");
+            foreach (var csalad in curieCsalad)
+            {
+                Console.WriteLine($"{csalad.Evszam}: {csalad.KeresztNev}- {csalad.VezetekNev} ({csalad.Tipus})");
+            }
+            var tipusok = adatok.Select(x => x.Tipus).Distinct().ToList();
+            foreach (var tipus in tipusok)
+            {
+                //  Console.WriteLine($"{tipus.Tipus} {tipus.Tipus.Count()}");
+            }
+            var orvosi = adatok.Where(x => x.Tipus == "orvosi").OrderBy(x => x.Evszam);
+            using (StreamWriter writer = new StreamWriter("orvosi.txt"))
+            {
+                foreach (var item in orvosi)
+                {
+                    string adatsor = $"{item.Evszam} {item.KeresztNev} {item.VezetekNev}";
+                    writer.WriteLine(adatsor);
+
+                }
+            }
+            Console.WriteLine("8. feladat: orvosi.txt");
+
         }
     }
 }
